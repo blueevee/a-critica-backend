ac6f2c6a97e7665c8edcf44d6284e01c839353f5
Blueevee <evelyn.ferreira@solutis.com.br>
2024-06-20 22:48:38 -0300

    feat: handling user sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :reset_session_expiration
+
+  private
+
+  def reset_session_expiration
+    request.session_options[:renew] = true
+  end
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: 'a_critica_backend', expire_after: 1.hour
